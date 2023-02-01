90f72c50f32904620b9d6e861cfd8b6c723c3e0d
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2023-02-01 11:50:26 +0100

    working count

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import *
+import PyPDF2
+
 import os
 import os.path
 import sys
@@ -37,15 +39,50 @@ class MainWindow(QMainWindow, Ui_pdfcount):
             self.addFileToTable(f)
 
     def addFileToTable(self, f):
+        if not is_pdf(f): return
+
+        self.removeTotalsRow()
+
         r = self.tblFiles.rowCount()
         self.tblFiles.setRowCount(r + 1)
-        self.tblFiles.setItem(r, 0, QTableWidgetItem(os.path.basename(f)))
+        self.tblFiles.setItem(r, 0, FileItem(f))
         self.tblFiles.setItem(r, 1, QTableWidgetItem(get_size(f)))
+        self.tblFiles.setItem(r, 2, QTableWidgetItem(""))
 
     def countPages(self):
+        self.removeTotalsRow()
+
+        sum = 0
         for i in range(self.tblFiles.rowCount()):
             print(self.tblFiles.item(i, 0).text())
-        pass
+            if self.tblFiles.item(i, 2).text() == "":
+                with open(self.tblFiles.item(i, 0).f, 'rb') as file:
+                    pdfReader = PyPDF2.PdfReader(file)
+                    count = len(pdfReader.pages)
+                    self.tblFiles.item(i, 2).setText(str(count))
+                sum = sum + count
+            else:
+                sum = sum + int(self.tblFiles.item(i, 2).text())
+
+        self.addTotalsRow(sum)
+
+    def addTotalsRow(self, sum):
+        r = self.tblFiles.rowCount()
+        self.tblFiles.setRowCount(r + 1)
+        self.tblFiles.setItem(r, 0, QTableWidgetItem("Total:"))
+        self.tblFiles.setItem(r, 2, QTableWidgetItem(str(sum)))
+
+    def removeTotalsRow(self):
+        r = self.tblFiles.rowCount()
+        if r > 0:
+            if self.tblFiles.item(r-1, 0).text() == "Total:":
+                self.tblFiles.removeRow(r-1)
+
+
+class FileItem(QTableWidgetItem):
+    def __init__(self, f):
+        super().__init__(os.path.basename(f))
+        self.f = f
 
 
 def get_size(path):
@@ -58,6 +95,11 @@ def get_size(path):
         return f"{round(size/(pow(1024,2)), 2)} MB"
     elif size < pow(1024, 4):
         return f"{round(size/(pow(1024,3)), 2)} GB"
+
+
+def is_pdf(f):
+    file_name, file_extension = os.path.splitext(f)
+    return file_extension.lower() == ".pdf"
 
 
 if __name__ == '__main__':
